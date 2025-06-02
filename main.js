40279fc82ab99e7e15778d9c1549e1632ad75503
Ujjwal Singh <uj643570ujjwal@gmail.com>
2025-06-02 12:32:17 +0530

    working hero card

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const APIKey = "d9e2f08a5cbb73d08a34ce3d5739a56b";
 const APIurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const inputBox = document.querySelector("form__group input");
+const inputBtn = document.getElementById("btn");
 
 async function checkweather(city) {
     const response = await fetch(APIurl + city + `&appid=${APIKey}`);
@@ -12,4 +14,8 @@ async function checkweather(city) {
     document.getElementById("highTemp").innerHTML = "High:  " + Math.round(output.main.temp_max)+"°"; 
     document.getElementById("lowTemp").innerHTML = "Low:  " + Math.round(output.main.temp_min)+"°"; 
 }
-checkweather();
+
+
+inputBtn.addEventListener("click", ()=>{
+    checkweather(inputBox.value);
+})
